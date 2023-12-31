28529bc67d38a440b799e81aaf1bc4088994303a
Frederick Pek <ssixthlhm@gmail.com>
2023-12-31 19:29:05 +0800

    rid of googleapiclient

--- blockchain/utils/GoogleSheetsClient.py
+++ blockchain/utils/GoogleSheetsClient.py
@@ -1,5 +1,11 @@
-from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
+import jwt
+import json
+import requests
+from datetime import datetime, timedelta
+
+GET, PUT = "GET", "PUT"
+BASE_URL = "https://sheets.googleapis.com/v4/spreadsheets"
+SPREADSHEET_ID_VALUES_RANGE = "/{}/values/{}"
 
 
 class GoogleSheetsClient:
@@ -7,35 +13,84 @@ class GoogleSheetsClient:
     Googlesheets Service Account Authenticated Api Client
     """
 
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-
     def __init__(
-        self, credentials_path: str, spreadsheet_id: str = None, sheet_id: str = None
+        self,
+        service_account_json_file: str,
+        spreadsheet_id: str = None,
+        sheet_id: str = None,
     ):
-        credentials = Credentials.from_service_account_file(
-            filename=credentials_path, scopes=self.scopes
-        )
-        service = build("sheets", "v4", credentials=credentials)
-        self.speadsheets_engine = service.spreadsheets().values()
+        self.access_token = self.generate_access_token(service_account_json_file)
         self.spreadsheet_id = spreadsheet_id
         self.sheet_id = sheet_id
 
-    def get_query_kwargs(
-        self, range: str, spreadsheet_id: str = None, sheet_id: str = None
+    @staticmethod
+    def generate_access_token(service_account_json_file):
+        params = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            "assertion": GoogleSheetsClient.generate_jwt_token(
+                service_account_json_file
+            ),
+        }
+        response = requests.post("https://oauth2.googleapis.com/token", data=params)
+        result = response.json()
+        if "access_token" not in result:
+            print(response, result)
+        return result["access_token"]
+
+    @staticmethod
+    def generate_jwt_token(service_account_json_file: str):
+        with open(service_account_json_file, "r") as file:
+            credentials = json.load(file)
+        iat = datetime.utcnow()
+        exp = iat + timedelta(minutes=60)
+        private_key = credentials["private_key"]
+        jwt_header = {"alg": "RS256", "typ": "JWT"}
+        jwt_payload = {
+            "scope": "https://www.googleapis.com/auth/spreadsheets",
+            "aud": "https://oauth2.googleapis.com/token",
+            "iss": credentials["client_email"],
+            "iat": iat,
+            "exp": exp,
+        }
+        jwt_token = jwt.encode(
+            jwt_payload, private_key, algorithm="RS256", headers=jwt_header
+        )
+        return jwt_token
+
+    def get_headers(self):
+        return {"Authorization": f"Bearer {self.access_token}"}
+
+    def request(
+        self, method: str, endpoint: str, query_params: dict = None, body: dict = None
     ) -> dict:
+        url = BASE_URL + endpoint
+        query_params = query_params or {}
+        headers = self.get_headers()
+        if method == GET:
+            resp = requests.get(url, headers=headers, params=query_params)
+        elif method == PUT:
+            resp = requests.put(url, headers=headers, params=query_params, json=body)
+        else:
+            raise ValueError(f"Unsupported method={method}")
+        return resp.json()
+
+    def parse_spreadsheet_id_and_range(
+        self, range: str, spreadsheet_id: str = None, sheet_id: str = None
+    ):
         sheet_id = sheet_id or self.sheet_id
         spreadsheet_id = spreadsheet_id or self.spreadsheet_id
         if sheet_id:
             range = f"{self.sheet_id}!{range}"
-        return {"spreadsheetId": spreadsheet_id, "range": range}
+        return spreadsheet_id, range
 
     def get_range(
         self, range: str, spreadsheet_id: str = None, sheet_id: str = None
     ) -> list:
-        kwargs = self.get_query_kwargs(
-            range, spreadsheet_id=spreadsheet_id, sheet_id=sheet_id
+        spreadsheet_id, range = self.parse_spreadsheet_id_and_range(
+            range, spreadsheet_id, sheet_id
         )
-        result = self.speadsheets_engine.get(**kwargs).execute()
+        endpoint = SPREADSHEET_ID_VALUES_RANGE.format(spreadsheet_id, range)
+        result = self.request(GET, endpoint)
         return result.get("values", list())
 
     def get_cell(self, cell: str, spreadsheet_id: str = None, sheet_id: str = None):
@@ -47,16 +102,13 @@ class GoogleSheetsClient:
     def update_range(
         self, range: str, values: list, spreadsheet_id: str = None, sheet_id: str = None
     ) -> dict:
-        kwargs = self.get_query_kwargs(
-            range, spreadsheet_id=spreadsheet_id, sheet_id=sheet_id
+        spreadsheet_id, range = self.parse_spreadsheet_id_and_range(
+            range, spreadsheet_id, sheet_id
         )
-        kwargs.update(
-            {
-                "body": {"values": values},
-                "valueInputOption": "USER_ENTERED",
-            }
-        )
-        result = self.speadsheets_engine.update(**kwargs).execute()
+        endpoint = SPREADSHEET_ID_VALUES_RANGE.format(spreadsheet_id, range)
+        body = {"values": values}
+        query_params = {"valueInputOption": "USER_ENTERED"}
+        result = self.request(PUT, endpoint, query_params=query_params, body=body)
         return result
 
     def update_cell(
